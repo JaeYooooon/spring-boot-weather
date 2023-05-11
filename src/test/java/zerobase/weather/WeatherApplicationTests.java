b78ac3ded55216f69b4a20dbf66818e65bba6f39
JaeYooooon <dhy04050@naver.com>
2023-05-11 14:29:06 +0900

    jdbc test

--- src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -3,11 +3,11 @@ package zerobase.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 @SpringBootTest
 class WeatherApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
