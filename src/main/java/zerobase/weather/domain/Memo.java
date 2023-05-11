5d41a55f95cc7ba9bb1265ca3762d8731f81d99d
JaeYooooon <dhy04050@naver.com>
2023-05-11 14:52:15 +0900

    jpa test

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -5,11 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "memo")
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String text;
 }
