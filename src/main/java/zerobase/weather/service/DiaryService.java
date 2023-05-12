5f59ce7e12c9853cd755a59d4b25ce1866197b34
JaeYooooon <dhy04050@naver.com>
2023-05-12 13:32:20 +0900

    read weather diary function 날씨 일기 조회 API 구현 특정 날짜로 조회, 특정 날짜들의 기간으로 조회

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -15,6 +15,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -41,6 +42,16 @@ public class DiaryService {
         nowDiary.setDate(date);
         diaryRepository.save(nowDiary);
     }
+
+    public List<Diary> readDiary(LocalDate date) {
+        return diaryRepository.findAllByDate(date);
+    }
+
+
+    public List<Diary> readDiaries(LocalDate startDate, LocalDate endDate) {
+        return diaryRepository.findAllByDateBetween(startDate, endDate);
+    }
+
     private String getWeatherString(){
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
         try {
